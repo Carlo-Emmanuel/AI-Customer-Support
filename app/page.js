b4a85c12f3eeb8debe82377ececcb20d9acfb8d2
Carlo-Emmanuel <uycarlo43@gmail.com>
2024-08-09 12:11:13 -0700

    completed a barebones sidebar for page directory coming changes will: - include more detailed styling and functionality - code restructuring for code modularization
    possible changes:
    - bard implementation into second chatbot
    - anthropic implementation into third chatbot

--- app/page.js
+++ app/page.js
@@ -4,15 +4,29 @@ import Image from "next/image";
 import {
   Box,
   Button,
-  Flex,
-  Input,
-  Text,
   Typography,
   Stack,
   TextField,
+  AppBar,
+  Toolbar,
+  IconButton,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Container,
+  Drawer,
+  CssBaseline,
+  List,
+  Divider,
+  Tooltip,
+  ListItemButton,
+  ListItem,
 } from "@mui/material";
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import App from "next/app";
+
+const pages = ["General", "Career Coach", "Resume Review"];
 
 export default function Home() {
   const [messages, setMessages] = useState([
@@ -21,7 +35,6 @@ export default function Home() {
       content: `Hi! I am the Headstarter Support Agent. How may I assist you today?`,
     },
   ]);
-
   const [message, setMessage] = useState("");
 
   const sendMessage = async () => {
@@ -64,69 +77,137 @@ export default function Home() {
     });
   };
 
+  const gptList = ['Headstarter AI Support', 'Career Coach', 'Resume Review'];
+
+  const drawerWidth = 244;
+
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Typography variant="h4">Headstarter AI Chat</Typography>
-      <Stack
-        direction="column"
-        width="800px"
-        height="700px"
-        border="1px solid black"
-        p={2}
-        spacing={3}
-        overflow="auto"
+    <>
+      <CssBaseline />
+      <Box sx={{ display: "flex" }}>
+        <AppBar
+          position="fixed"
+          sx={{
+            width: `calc(100% - ${drawerWidth}px)`,
+            ml: `${drawerWidth}px`,
+          }}
+        >
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Headstarter AI Support
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <Divider />
+
+          <List>
+            {gptList.map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+
+          <List>
+            {pages.map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Box>
+      <Toolbar />
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
       >
+        <Typography variant="h4">Headstarter AI Chat</Typography>
         <Stack
           direction="column"
-          maxWidth="100%"
-          spacing={2}
-          flexGrow={1}
+          width="800px"
+          height="700px"
+          border="1px solid black"
+          p={2}
+          spacing={3}
           overflow="auto"
-          maxHeight="100%"
         >
-          {messages.map((msg, index) => (
-            // if its the assistant, it starts on the left and if its the user it starts on the right
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                msg.role === "assistant" ? "flex-start" : "flex-end"
-              }
-            >
+          <Stack
+            direction="column"
+            maxWidth="100%"
+            spacing={2}
+            flexGrow={1}
+            overflow="auto"
+            maxHeight="100%"
+          >
+            {messages.map((msg, index) => (
+              // if its the assistant, it starts on the left and if its the user it starts on the right
               <Box
-                bgcolor={
-                  // if its the assistant, primary color, if its the user, secondary color
-                  msg.role === "assistant" ? "info.main" : "secondary.main"
+                key={index}
+                display="flex"
+                justifyContent={
+                  msg.role === "assistant" ? "flex-start" : "flex-end"
                 }
-                color="white"
-                borderRadius={16}
-                p={3}
-                maxWidth="75%"
               >
-                <ReactMarkdown>{msg.content}</ReactMarkdown>
+                <Box
+                  bgcolor={
+                    // if its the assistant, primary color, if its the user, secondary color
+                    msg.role === "assistant" ? "info.main" : "secondary.main"
+                  }
+                  color="white"
+                  borderRadius={16}
+                  p={3}
+                  maxWidth="75%"
+                >
+                  <ReactMarkdown>{msg.content}</ReactMarkdown>
+                </Box>
               </Box>
-            </Box>
-          ))}
+            ))}
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <TextField
+              label="Message"
+              fullWidth
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            {
+              //if text field is empty, button is hidden so user can't send empty messages
+              message !== "" && (
+                <Button
+                  variant="contained"
+                  hidden={message != "" ? false : true}
+                  onClick={sendMessage}
+                >
+                  Send
+                </Button>
+              )
+            }
+          </Stack>
         </Stack>
-        <Stack direction="row" spacing={2}>
-          <TextField
-            label="Message"
-            fullWidth
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <Button variant="contained" onClick={sendMessage}>
-            Send
-          </Button>
-        </Stack>
-      </Stack>
-    </Box>
+      </Box>
+    </>
   );
 }
